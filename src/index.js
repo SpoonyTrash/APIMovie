e1b8ec602a41b7e34e3128df3d6ebc3c88177dbe
Spoony <dalexismc17@gmail.com>
2022-06-16 21:05:36 -0500

    Fixed bug: duplicate data upload

--- src/index.js
+++ src/index.js
@@ -14,8 +14,9 @@ export async function getTrendingMoviesPreview(){
     const {data} = await api('trending/movie/day');
     
     const movies = data.results
+
+    trendingMoviesPreviewList.innerHTML = '';
     movies.forEach(movie => {
-    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
       const movieContainer = document.createElement('div');
       movieContainer.classList.add('movie-container');
@@ -35,8 +36,9 @@ export async function getCategoriesPreview(){
   const {data} = await api('genre/movie/list');
   
   const categories = data.genres
-  categories.forEach(category => {
-  const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+  categoriesPreviewList.innerHTML = '';
+  categories.forEach(category => {  
 
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
